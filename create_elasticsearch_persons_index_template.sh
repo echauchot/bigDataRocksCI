9c3431d95ac6a6488d87664bee21120d37cc6b3b
Etienne Chauchot <etienne.chauchot@free.fr>
2016-05-21 18:01:46 +0200

    hardcode template name and filename because this script is for a particular index

--- create_elasticsearch_persons_index_template.sh
+++ create_elasticsearch_persons_index_template.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-ES_HOST="$3"
+ES_HOST="$1"
 if [ -z "$ES_HOST" ]; then ES_HOST="localhost"; fi
-ES_PORT="$4"
+ES_PORT="$2"
 if [ -z "$ES_PORT" ]; then ES_PORT="9200"; fi
 
 ES_URL="http://${ES_HOST}:${ES_PORT}"
 
 # ES template to create
-ES_TEMPLATE="$1"
+ES_TEMPLATE="persons"
 
 # mapping for this index
-TEMPLATE_FILE="$2"
+TEMPLATE_FILE="../bigDataRocks/common/src/main/resources/bigdatarocks/common/configuration/elasticsearch_persons_index_template.json"
 if [ -f "$TEMPLATE_FILE" ]; then
     echo
     echo "$(date '+%F %H:%M:%S') INFO: BEGIN creating template $ES_TEMPLATE and applying template file $TEMPLATE_FILE"
